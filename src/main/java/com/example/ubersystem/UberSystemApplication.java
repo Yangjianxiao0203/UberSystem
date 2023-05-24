9303edb864d8b537fa392cdc234c8746bf306cad
Yangjianxiao0203 <yangjx@bu.edu>
2023-05-24 10:07:57 -0400

    initial

---
+++ src/main/java/com/example/ubersystem/UberSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ubersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ubersystem/UberSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ubersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
